aa5137e0113079cc9c6f310fcc36f6390aee200c
Sumukh Prasad <sumukhprasad@icloud.com>
2021-09-18 11:01:06 +0530

    Re-added bot files, stopped rickrolling with the About command.

--- src/main/bot/commands/help.js
+++ src/main/bot/commands/help.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js")
 exports.run = (client, message, args) => ***REMOVED***
     const helpEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Usage')
+	.setColor('#ff1111')
+	.setTitle('Rhapsody')
 	.addFields(
-		***REMOVED*** name: 'Never', value: 'Gonna Give You Up' ***REMOVED***
+		***REMOVED*** name: 'Something.', value: 'Something else.' ***REMOVED***
 	)
     message.channel.send(helpEmbed).catch(console.error);
 ***REMOVED***
