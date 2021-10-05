ad5f4f06cbe72cc4f2ca890866aa7b7b2dc29211
Anubhav <anubhav.shyjesh@gmail.com>
2021-10-05 16:41:32 +0530

    Added all playlists servlet (without functionality)

--- src/main/backend/packages/routes/playlistRoutes.py
+++ src/main/backend/packages/routes/playlistRoutes.py
@@ -25,3 +25,79 @@ def createPlaylist():
         response = {
             "status": 500
         }
+
+@playlistRoutes.route("/removePlaylist")
+def removePlaylist():
+    args = request.args
+    guild_id = args.get("g")
+    playlistName = args.get("n")
+
+    try:
+        response = {
+            "status": 200
+        }
+
+        return jsonify(response)
+
+    except Exception as e:
+        response = {
+            "status": 500
+        }
+
+
+@playlistRoutes.route("/addToPlaylist")
+def addToPlaylist():
+    args = request.args
+    guild_id = args.get("g")
+    playlistName = args.get("n")
+    term = args.get("t")
+
+    try:
+        response = {
+            "status": 200
+        }
+
+        return jsonify(response)
+
+    except Exception as e:
+        response = {
+            "status": 500
+        }
+
+@playlistRoutes.route("/removeFromPlaylist")
+def removeFromPlaylist():
+    args = request.args
+    guild_id = args.get("g")
+    playlistName = args.get("n")
+    position = args.get("p")
+
+    try:
+        response = {
+            "status": 200
+        }
+
+        return jsonify(response)
+
+    except Exception as e:
+        response = {
+            "status": 500
+        }
+
+
+@playlistRoutes.route("/addPlaylistToQueue")
+def addPlaylistToQueue():
+    args = request.args
+    guild_id = args.get("g")
+    playlistName = args.get("n")
+
+    try:
+        response = {
+            "status": 200
+        }
+
+        return jsonify(response)
+
+    except Exception as e:
+        response = {
+            "status": 500
+        }
