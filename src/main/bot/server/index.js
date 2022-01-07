64482463f1e712940ff6361b379001a0af11d0ba
Sumukh Prasad <sumukhprasad@icloud.com>
2022-01-07 20:45:42 +0530

    Added announcement script.

--- src/main/bot/server/index.js
+++ src/main/bot/server/index.js
@@ -3,6 +3,12 @@ var app = express();
 const chalk = require('chalk');
 const getServerInfo = require('./server-modules/aboutServer.js');
 const getRhapsodyInfo = require('./server-modules/aboutThisRhapsody.js');
+const announce = require('./server-modules/announce.js');
+
+app.all('*', function (req, res, next) {
+     res.header('Access-Control-Allow-Origin', '*');
+     next();
+});
 
 app.get('/aboutServer', function (req, res) {
 	getServerInfo().then(value => res.end(JSON.stringify(value)));
@@ -10,6 +16,10 @@ app.get('/aboutServer', function (req, res) {
 
 app.get('/aboutThisRhapsody', function (req, res) {
 	getRhapsodyInfo().then(value => res.end(JSON.stringify(value)));
+});
+
+app.get('/announce', function (req, res) {
+	announce((req.query.title || "Announcement"), (req.query.description || "*Oops, something went wrong with the announcement script. Sorry!*")).then(value => res.end(JSON.stringify(value)));
 });
 
 function start(port) {
