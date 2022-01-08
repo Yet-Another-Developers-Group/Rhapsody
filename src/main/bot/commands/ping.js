c50bed95fb370e03670c33dc2fc43aec73e61aa1
Sumukh Prasad <sumukhprasad@icloud.com>
2022-01-08 11:14:57 +0530

    Added documentation for commands/ping.js

--- src/main/bot/commands/ping.js
+++ src/main/bot/commands/ping.js
@@ -1,6 +1,12 @@
 require('../assets/ExtendedMessage.js');
+
+/**
+ * Ping/pong
+ * @param {Discord.Client} client 
+ * @param {Discord.Message} message 
+ * @returns 
+ */
 exports.run = async (client, message) => {
-	
 	message.inlineReply('Pong!');
 };
 
