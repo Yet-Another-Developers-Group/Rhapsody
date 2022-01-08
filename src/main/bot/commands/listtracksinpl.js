589dbe67985782617a43c6332eeeb0a29d07612f
Sumukh Prasad <sumukhprasad@icloud.com>
2022-01-08 11:13:49 +0530

    Added documentation for commands/listtracksinpl.js

--- src/main/bot/commands/listtracksinpl.js
+++ src/main/bot/commands/listtracksinpl.js
@@ -1,6 +1,14 @@
 const Discord = require('discord.js');
 const defaultEmbedColor = require('../config.json').defaultEmbedColor;
 require('../ExtendedMessage/ExtendedMessage');
+
+/**
+ * Lists tracks in a playlist.
+ * @param {Discord.Client} client 
+ * @param {Discord.Message} message 
+ * @param {array} args 
+ * @returns 
+ */
 exports.run = (client, message, args) => {
 	if(!args || args.length < 1) return message.inlineReply('I\'m sorry, I didn\'t understand that.');
 	const embed = new Discord.MessageEmbed()
