06e2f923d6b9aeeda03e5b133113ec3c9f5cf29a
Sumukh Prasad <sumukhprasad@icloud.com>
2022-01-29 20:06:25 +0530

    Fixed locking problem.

--- src/main/bot/commands/lock.js
+++ src/main/bot/commands/lock.js
@@ -10,9 +10,9 @@ require('../ExtendedMessage/ExtendedMessage');
 exports.run = async (client, message) => {
 	if(!message.member.voice.channel || typeof message.member.voice.channel == 'undefined') return message.inlineReply('You must be in a Voice Channel to use this command.');
 	if(!queues[message.guild.id]) return message.inlineReply('I\'m not playing anything here at the moment. Use the `queue` or `play` command to add more songs to the queue.');
-	if(locks[message.guild.id].isLocked && locks[message.guild.id].userID != message.author.id && locks[message.guild.id].allowedUsers.indexOf('<@!'+message.author.id+'>') > -1 ) return message.inlineReply('This player is currently locked by <@!'+locks[message.guild.id].userID+'>.');
+	if(locks[message.guild.id] && locks[message.guild.id].isLocked && locks[message.guild.id].userID != message.author.id && locks[message.guild.id].allowedUsers.indexOf('<@!'+message.author.id+'>') > -1 ) return message.inlineReply('This player is currently locked by <@!'+locks[message.guild.id].userID+'>.');
 
-	var isLocked = locks[message.guild.id].lock(message.author.id);
+	var isLocked = locks[message.guild.id].lock(message.author.id, []);
 	if (isLocked) {
 		message.inlineReply('Locked. You can unlock by using the `-unlock` command (If you don\'t, this player will automatically unlock in 15 minutes.).');
 		(async function () {
--- src/main/bot/commands/unlock.js
+++ src/main/bot/commands/unlock.js
@@ -11,7 +11,7 @@ require('../ExtendedMessage/ExtendedMessage');
 exports.run = async (client, message) => {
 	if(!message.member.voice.channel || typeof message.member.voice.channel == 'undefined') return message.inlineReply('You must be in a Voice Channel to use this command.');
 	if(!queues[message.guild.id]) return message.inlineReply('I\'m not playing anything here at the moment. Use the `queue` or `play` command to add more songs to the queue.');
-	if(locks[message.guild.id].isLocked && locks[message.guild.id].userID != message.author.id && locks[message.guild.id].allowedUsers.indexOf('<@!'+message.author.id+'>') > -1 ) return message.inlineReply('This player is currently locked by <@!'+locks[message.guild.id].userID+'>.');
+	if( locks[message.guild.id] && locks[message.guild.id].isLocked && locks[message.guild.id].userID != message.author.id && locks[message.guild.id].allowedUsers.indexOf('<@!'+message.author.id+'>') > -1 ) return message.inlineReply('This player is currently locked by <@!'+locks[message.guild.id].userID+'>.');
 
 	if(!locks[message.guild.id]) return message.inlineReply('Already unlocked.');
 	var isLocked = locks[message.guild.id].unlock();
