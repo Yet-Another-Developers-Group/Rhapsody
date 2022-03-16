53923a6dd17636bd0a31e232c460b5354cd42172
Sumukh Prasad <sumukhprasad@icloud.com>
2022-03-16 20:10:09 +0530

    Improved help command.

--- src/main/bot/commands/help.js
+++ src/main/bot/commands/help.js
@@ -1,16 +1,45 @@
-const { MessageActionRow, MessageSelectMenu, MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const { defaultEmbedColor } = require('../config.json');
+const rHelpManager = require('../rHelpManager');
+const helpDocs = rHelpManager.requireHelpDocs();
 
-const run = (client, message) => {
-	message.reply({ content: '"The internet is full of K-Pop loving monkeys"\n- Sun Tzu (Developers)' });
+const run = (client, message, args) => {
+	const helpEmbed = new MessageEmbed()
+		.setColor(defaultEmbedColor)
+		.setFooter({text: `${helpDocs.version}, HelpDocs created on ${helpDocs.dateCreated}.`});
+
+	if (args.length < 1) {
+		helpEmbed
+			.setTitle(helpDocs.defaultPresets.defaultDialogIntroductionTitle)
+			.setDescription(helpDocs.defaultPresets.defaultDialogIntroductionDescription);
+		return message.reply({ embeds: [helpEmbed] });
+	}
+	if (args[0] == 'list') return message.reply({ content: `**Rhapsody Commands List**\n\`\`\`${helpDocs.docs.map(doc => `-${doc.id}`)}\`\`\`` });
+
+	const helpDoc = helpDocs.docs.filter(doc => doc.id == args[0])[0];
+
+	if (!helpDoc) {
+		helpEmbed
+			.setTitle(helpDocs.defaultPresets.defaultDialogIntroductionTitle)
+			.setDescription(helpDocs.defaultPresets.defaultNotFoundMessageDescription.replace('%REQUESTEDNAME%', args[0]));
+	} else {
+		helpEmbed
+			.setTitle(helpDocs.defaultPresets.defaultTitleSchema.replace('%COMMANDNAME%', helpDoc.name))
+			.setDescription(helpDocs.defaultPresets.defaultDescriptionSchema
+				.replace('%COMMANDDESCRIPTION%', helpDoc.desc)
+				.replace('%COMMANDSYNTAX%', helpDoc.commandSyntax)
+				.replace('%COMMANDSHORTCUTS%', helpDoc.shortcuts));
+	}
+	message.reply({ embeds: [helpEmbed] });
 };
 
-const shortcuts = ['h'];
+const shortcuts = ['h', 'showhelp', 'commands'];
 
 const helpDoc = {
 	name: 'Help',
-	commandSyntax: '-help',
-	shortcuts: shortcuts.map(i => '-'+i).join()
+	desc: 'Provides help on commands/functions.',
+	commandSyntax: '-help <command>',
+	shortcuts: shortcuts.map(i => '-'+i).join(', ')
 };
 
 module.exports = {
--- src/main/bot/rHelpManager/index.js
+++ src/main/bot/rHelpManager/index.js
@@ -7,6 +7,7 @@ class rHelpManager {
 		var helpDocJson = {
 			version: `Version ${require('../package.json').fullVersion}`,
 			docs: [],
+			defaultPresets: require('./helpDocsPresets.json'),
 			dateCreated: '?'
 		};
 		fs.readdir(__dirname + '/../commands/', (err, files) => {
@@ -15,7 +16,8 @@ class rHelpManager {
 				if (!file.endsWith('.js')) return;
 				let helpDoc = require(__dirname + `/../commands/${file}`).helpDoc;
                     
-				process.send(chalk.yellow.bold('[Loading HelpDoc]') + ` ${helpDoc.name}...`);
+				process.send(chalk.yellow.bold('[Loading HelpDoc]') + ` ${file.split('.')[0]}...`);
+				helpDoc.id = file.split('.')[0];
 				helpDocJson.docs.push(helpDoc);
 			});
 
@@ -23,6 +25,10 @@ class rHelpManager {
 			fs.writeFileSync(__dirname+'/helpDocs.json', JSON.stringify(helpDocJson));
 			process.send(chalk.yellow.bold('[HelpDoc Notice]') + ' HelpDoc was created.');
 		});
+	}
+
+	static requireHelpDocs() {
+		return require('./helpDocs.json');
 	}
 }
 
