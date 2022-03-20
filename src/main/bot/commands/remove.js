9c403409434530f01ded9c63e228cc83adad73a0
Sumukh Prasad <sumukhprasad@icloud.com>
2022-03-20 18:31:08 +0530

    Fixed bug in `commands/remove.js`.

--- src/main/bot/commands/remove.js
+++ src/main/bot/commands/remove.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const defaultEmbedColor = require('../config.json').defaultEmbedColor;
 
-exports.run = (client, message, args) => {
+const run = (client, message, args) => {
 	if(!args || args.length < 1) return message.reply('I\'m sorry, I didn\'t understand that.');
 	const embed = new Discord.MessageEmbed()
 		.setColor(defaultEmbedColor)
