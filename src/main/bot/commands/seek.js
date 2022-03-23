10a148febec84d8d86abb77195d5231b88f9045b
Sumukh Prasad <sumukhprasad@icloud.com>
2022-03-23 16:53:13 +0530

    Added seek functionality.

--- src/main/bot/commands/seek.js
+++ src/main/bot/commands/seek.js
@@ -12,7 +12,48 @@ const run = async (client, message, args) => {
 	if(!queues[message.guild.id]) return message.reply('I\'m not playing anything here at the moment. Use the `queue` or `play` command to add more songs to the queue.');
 	if(queues[message.guild.id].currentlyPlaying.info.isStream) return message.reply('Sorry, this command does not work on Live Streams.');
 
-	message.reply('SEEK TO ' + args.join(' '));
+	const timeString = args.join(' ');
+
+
+	const unitMap = [
+		{ 
+			possibleValues: ["ms", "milli", "millisecond", "milliseconds"],
+			numberOfMilliseconds: 1
+		},
+		{ 
+			possibleValues: ["s", "sec", "secs", "second", "seconds"],
+			numberOfMilliseconds: 1000
+		},
+		{ 
+			possibleValues: ["m", "min", "mins", "minute", "minutes"],
+			numberOfMilliseconds: 60000
+		},
+		{ 
+			possibleValues: ["h", "hr", "hrs", "hour", "hours"],
+			numberOfMilliseconds: 600000
+		},
+	];
+
+
+	const groups = timeString
+		.toLowerCase()
+		.match(/[0-9]+[a-z]+/g);
+
+	if (groups == null || groups == []) return message.reply('Please specify a seek time after the command like this:\n`-seek <time>`');
+
+	var totalMS = 0;
+	for (let i = 0; i < groups.length; i++) {
+		const group = groups[i];
+		const value = group.match(/[0-9]+/g)[0];
+		const unit = group.match(/[a-z]+/g)[0];
+		const unitObject = unitMap.filter(i => i.possibleValues.indexOf(unit) > -1)[0];
+		if (typeof unitObject == 'undefined') return message.reply(`Sorry, I couldn\'t understand this time specification: \n\`${group}\``);
+		totalMS += value*unitObject.numberOfMilliseconds;
+	}
+	
+	queues[message.guild.id].seek(totalMS);
+
+	message.reply('Seeked.');
 };
 
 const shortcuts = [];
