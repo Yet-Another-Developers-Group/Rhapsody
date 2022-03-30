32a6fa370fa95241569b615bc0c72a3e021b122a
Anubhav <anubhav.shyjesh@gmail.com>
2022-03-30 17:53:43 +0530

    Added JSON Outputs to current scripts

--- src/main/bot/scripts/playlists/removePlaylist.py
+++ src/main/bot/scripts/playlists/removePlaylist.py
@@ -1,9 +1,10 @@
 import argparse
-
+import json
+import pymongo
 from pymongo import MongoClient
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--server_id", help="Server ID for which the playlist is to be removed")
+parser.add_argument("-g", "--server_id", help="Server ID for which the playlist is to be removed")
 parser.add_argument("-n", "--name", help="Name of the Playlist to be removed")
 
 args = parser.parse_args()
@@ -11,22 +12,37 @@ args = parser.parse_args()
 client = MongoClient()
 db = client["Rhapsody"]
 
-server_list = db["info"]
+try:
+    server_list = db["info"]
 
-playlist_id = args.server_id + "-" + args.name
-server_info = server_list.find_one({"_id" : args.server_id})
+    playlist_id = args.server_id + "-" + args.name
+    server_info = server_list.find_one({"_id" : args.server_id})
 
-if args.name in server_info["list_playlists"]:
+    if args.name in server_info["list_playlists"]:
 
-    server_info["num_playlists"] -= 1
-    server_info["list_playlists"].remove(args.name)
+        server_info["num_playlists"] -= 1
+        server_info["list_playlists"].remove(args.name)
 
-    server_list.update_one({"_id" : args.server_id}, {"$set" : server_info})
+        server_list.update_one({"_id" : args.server_id}, {"$set" : server_info})
 
-    playlist = db[playlist_id]
-    playlist.drop()
+        playlist = db[playlist_id]
+        playlist.drop()
+
+        print(json.dumps({
+            "guildid": args.server_id,
+            "name": args.name
+        }))
 
 
-else:
-    print(f"The Playlist {args.name} does not exist in this server")
-    exit()
+    else:
+        print(json.dumps({
+            "ecode": "E-2001"
+        }))
+        
+        exit()
+
+except pymongo.errors.PyMongoError as e:
+
+    print(json.dumps({
+        "ecode": "E-2003"
+    }))
