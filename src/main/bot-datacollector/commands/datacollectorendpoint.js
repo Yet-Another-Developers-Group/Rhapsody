9ea4c596385f260008bf3ea51226c0006187254a
Sumukh Prasad <sumukhprasad@icloud.com>
2022-04-14 07:58:22 +0530

    Added basic data collection functionality.

--- src/main/bot-datacollector/commands/datacollectorendpoint.js
+++ src/main/bot-datacollector/commands/datacollectorendpoint.js
@@ -1,6 +1,13 @@
-const run = async (client, message, args) => {
-	data = args.join(' ').split('-');
-	message.reply(`Question: ${data[0]}\nExpected: ${data[1]}`);
+const run = async (client, m, args) => {
+	data = args.join(' ').split('-').map(i => i.trim());
+	const message = await m.reply('Data recieved...');
+
+	if (data.length < 2 ||
+		typeof data[1] == 'undefined' ||
+		data[0] == '' ||
+		data[1] == '') return message.edit('Sorry, RDC could not understand your submission. Please use the following syntax to submit data:\n `*datacollectorendpoint <submissiondata> - <identifier>`')
+
+	message.edit(`Understood given data as:\nQuestion: \`${data[0]}\`\nExpected HelpDoc: \`${data[1]}\``);
 };
 
 const shortcuts = ['dcep', 'adddata', 'newdata'];
@@ -8,8 +15,8 @@ const shortcuts = ['dcep', 'adddata', 'newdata'];
 const helpDoc = {
 	name: 'Data Collector Endpoint',
 	desc: 'Allows to add new data.',
-	commandSyntax: '-datacollectorendpoint <submissiondata> - <identifier>',
-	shortcuts: shortcuts.map(i => '`-'+i+'`').join(', ')
+	commandSyntax: '*datacollectorendpoint <submissiondata> - <identifier>',
+	shortcuts: shortcuts.map(i => '`*'+i+'`').join(', ')
 };
 
 module.exports = {
