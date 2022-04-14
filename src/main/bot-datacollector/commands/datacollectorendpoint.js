452efb58e7fe9b984bdc45767883463f38c5af72
Sumukh Prasad <sumukhprasad@icloud.com>
2022-04-14 13:04:24 +0530

    Added `rDataCollectionManager` functions.

--- src/main/bot-datacollector/commands/datacollectorendpoint.js
+++ src/main/bot-datacollector/commands/datacollectorendpoint.js
@@ -1,13 +1,21 @@
+const rDataCollectionManager = require("../rDataCollectionManager");
+
 const run = async (client, m, args) => {
-	data = args.join(' ').split('-').map(i => i.trim());
+	data = args.join(' ').split('-').map(i => i.trim().replace(/[^a-zA-Z0-9]/g, ''));
 	const message = await m.reply('Data recieved...');
 
 	if (data.length < 2 ||
 		typeof data[1] == 'undefined' ||
 		data[0] == '' ||
-		data[1] == '') return message.edit('Sorry, RDC could not understand your submission. Please use the following syntax to submit data:\n `*datacollectorendpoint <submissiondata> - <identifier>`')
+		data[1] == '') return message.edit('Sorry, RDC could not understand your submission. Please use the following syntax to submit data:\n `*datacollectorendpoint <submissiondata> - <identifier>`');
 
-	message.edit(`Understood given data as:\nQuestion: \`${data[0]}\`\nExpected HelpDoc: \`${data[1]}\``);
+	const isValid = rDataCollectionManager.checkIfIdentifierIsValid(data[1]);
+	if (!isValid) return message.edit(`Sorry, RDC could not find the HelpDoc for "${data[1]}." Please try again.`);
+
+	await message.edit(`Understood given data as:\nQuestion: \`${data[0]}\`\nExpected HelpDoc: \`${data[1]}\`\n*Please note that all data submissions are first sanitised. Special characters you may have entered will **not** appear in the results.*`);
+
+	const hasBeenPutIntoDatabase = await rDataCollectionManager.addDataAndIdentifierArrayToDatabase(data[0], data[1]);
+	if (!hasBeenPutIntoDatabase) return message.edit(`Sorry, RDC could encountered an error while trying to add your submission to the database. Please ask your bot administrator to check the logs and fix the issue.`);
 };
 
 const shortcuts = ['dcep', 'adddata', 'newdata'];
--- src/main/bot-datacollector/rDataCollectionManager/index.js
+++ src/main/bot-datacollector/rDataCollectionManager/index.js
@@ -1,5 +1,13 @@
 class rDataCollectionManager {
+     static checkIfIdentifierIsValid(identifier) {
+          const identifiers = require('../../bot/rHelpManager/helpDocs.json').docs.map(i => i.id);
+          if (identifiers.indexOf(identifier) < 0) return false;
+          return true;
+     }
 
+     static async addDataAndIdentifierArrayToDatabase(data, id) {
+          return false;
+     }
 }
 
 module.exports = rDataCollectionManager;
