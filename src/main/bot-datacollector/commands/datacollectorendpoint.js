8e765f862b17c22c58f41abdb1df276356535d5a
Sumukh Prasad <sumukhprasad@icloud.com>
2022-04-16 16:03:30 +0530

    `rDataCollectionManager` improvements.

--- src/main/bot-datacollector/commands/datacollectorendpoint.js
+++ src/main/bot-datacollector/commands/datacollectorendpoint.js
@@ -1,7 +1,7 @@
 const rDataCollectionManager = require("../rDataCollectionManager");
 
 const run = async (client, m, args) => {
-	data = args.join(' ').split('-').map(i => i.trim().replace(/[^a-zA-Z0-9]/g, ''));
+	data = args.join(' ').split('-').map(i => i.trim().replace(/[^a-zA-Z ]/g, ''));
 	const message = await m.reply('Data recieved...');
 
 	if (data.length < 2 ||
@@ -9,13 +9,12 @@ const run = async (client, m, args) => {
 		data[0] == '' ||
 		data[1] == '') return message.edit('Sorry, RDC could not understand your submission. Please use the following syntax to submit data:\n `*datacollectorendpoint <submissiondata> - <identifier>`');
 
-	const isValid = rDataCollectionManager.checkIfIdentifierIsValid(data[1]);
-	if (!isValid) return message.edit(`Sorry, RDC could not find the HelpDoc for "${data[1]}." Please try again.`);
-
 	await message.edit(`Understood given data as:\nQuestion: \`${data[0]}\`\nExpected HelpDoc: \`${data[1]}\`\n*Please note that all data submissions are first sanitised. Special characters you may have entered will **not** appear in the results.*`);
 
-	const hasBeenPutIntoDatabase = await rDataCollectionManager.addDataAndIdentifierArrayToDatabase(data[0], data[1]);
+	const hasBeenPutIntoDatabase = await rDataCollectionManager.addDataAndIdentifierArrayToDatabase(data[0], data[1], m.author.tag);
 	if (!hasBeenPutIntoDatabase) return message.edit(`Sorry, RDC could encountered an error while trying to add your submission to the database. Please ask your bot administrator to check the logs and fix the issue.`);
+
+	message.edit(`Your submission was collected. Thanks a bunch for helping out!\nQuestion: \`${data[0]}\`\nExpected HelpDoc: \`${data[1]}\``);
 };
 
 const shortcuts = ['dcep', 'adddata', 'newdata'];
--- src/main/bot-datacollector/rDataCollectionManager/index.js
+++ src/main/bot-datacollector/rDataCollectionManager/index.js
@@ -1,13 +1,13 @@
 const collectedDataModel = require('../rCollectedDataModel/model.js');
 
 class rDataCollectionManager {
-     static checkIfIdentifierIsValid(identifier) {
+     static async addDataAndIdentifierArrayToDatabase(d, id, uname) {
           const identifiers = require('../../bot/rHelpManager/helpDocs.json').docs.map(i => i.id);
-          if (identifiers.indexOf(identifier) < 0) return false;
-          return true;
-     }
-
-     static async addDataAndIdentifierArrayToDatabase(d, id) {
+          if (identifiers.indexOf(id) < 0) {
+               process.send(`HelpDoc not found!:\n User: ${uname}\nTrying to access: ${id}\n`)
+               return false;
+          };
+          
           let newData = new collectedDataModel({
                commandIdentifier: id,
                data: d,
