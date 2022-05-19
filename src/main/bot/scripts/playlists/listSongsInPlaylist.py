f486a4b46854f54d5fa4f1f5383b3ae4ccbd3571
Anubhav <anubhav.shyjesh@gmail.com>
2022-05-19 15:31:17 +0530

    Finished getSongFromPlaylist script

--- src/main/bot/scripts/playlists/listSongsInPlaylist.py
+++ src/main/bot/scripts/playlists/listSongsInPlaylist.py
@@ -1,1 +1,38 @@
-# This is also most desperately needed.
+import argparse
+
+import pymongo
+from pymongo import MongoClient
+
+import json
+
+import base64
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--server_id", help="Server ID for the server in which the playlist of the song is in")
+parser.add_argument("-n", "--name", help="The playlist in which the song is in")
+
+args = parser.parse_args()
+
+client = MongoClient()
+db = client["rhapsody"]
+
+playlist_name = args.server_id + "-" + args.name
+
+playlist = db[playlist_name]
+
+try:
+    ### Find song in playlist collection
+    info = playlist.find_one({"_id": "info"})
+
+    ### Response
+    res = {
+        "guild_id": args.server_id,
+        "playlist": args.name,
+        "song": info["list_songs"]
+    }
+
+    res = json.dumps(res)
+    print(res)
+
+except Exception as e:
+    print(e)
--- src/main/bot/scripts/playlists/verifyPlaylistExistence.py
+++
@@ -1,1 +0,0 @@
-# This is most desperately needed.
