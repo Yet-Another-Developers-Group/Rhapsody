da7706cf533cc0d49eb51862bffb080cf537b351
Sumukh Prasad <sumukhprasad@icloud.com>
2022-06-11 08:56:59 +0530

    Added pagination to `removeduplicatetracks`.

--- src/main/bot/commands/removeduplicatetracks.js
+++ src/main/bot/commands/removeduplicatetracks.js
@@ -1,7 +1,21 @@
 const queues = require('../bot.js').queues;
 const locks = require('../bot.js').locks;
 const msToHMS = require('../rUtilities/rUtilities.js').millisecondsToHMSString;
+const { MessageActionRow, MessageButton } = require('discord.js');
 
+// PAGINATION - CONSTANTS
+const backId = 'back';
+const forwardId = 'forward';
+const backButton = new MessageButton({
+	style: 'SECONDARY',
+	label: 'Previous Page',
+	customId: backId
+});
+const forwardButton = new MessageButton({
+	style: 'SECONDARY',
+	label: 'Next Page',
+	customId: forwardId
+});
 /**
  * Removes duplicate tracks form the queue.
  * @param {Discord.client} client 
@@ -21,9 +35,49 @@ const run = async (client, message) => {
 
 	const duplicateTracks = await queues[message.guild.id].findDuplicateTracks();
 	
-	if (duplicateTracks.length < 1) return message.reply('No duplicates.');
+	//if (duplicateTracks.length < 1) return message.reply('No duplicates.');
 
-	message.reply(`Are you sure you want to remove duplicate tracks from the queue?\`\`\`${duplicateTracks.slice(0, 5).map((song, index) => `${++index}) ${song.info.title} - ${song.info.author} - ${msToHMS(song.info.length)}`).join('\n')}${duplicateTracks > 5 ? `\n\n... and ${duplicateTracks.length-5} more.` : ""}\`\`\`\n(Reply to this message with y/n to confirm.)`);
+	//message.reply(`Are you sure you want to remove duplicate tracks from the queue?\`\`\`${duplicateTracks.slice(0, 10).map((song, index) => `${++index}) ${song.info.title} - ${song.info.author} - ${msToHMS(song.info.length)}`).join('\n')}${duplicateTracks > 5 ? `\n\n... and ${duplicateTracks.length-10} more.` : ""}\`\`\`\n(Reply to this message with y/n to confirm.)`);
+
+	const pages = duplicateTracks.map((song, index) => `${++index}. ${song.info.title} - ${song.info.author} - (${song.info.isStream ? 'Live Stream' : msToHMS(song.info.length)})`).chunk(5);
+
+	if (pages.length == 0) return message.reply('No duplicates.');
+
+
+	let currentIndex = 0;
+	const queueMessage = await message.reply({
+		content: 'Are you sure you want to remove duplicate tracks from the queue?```'+pages[currentIndex].join('\n')+'```\n(Reply to this message with y/n to confirm.)',
+		components: pages.length < 2
+			? []
+			: [new MessageActionRow({components: [forwardButton]})]
+	});
+	if (pages.length < 2) return;
+
+	const collector = queueMessage.createMessageComponentCollector({
+		filter: ({user}) => user.id == message.author.id,
+		time: 10000
+	});
+	collector.on('collect', async interaction => {
+		// Increase/decrease index
+		interaction.customId === backId ? (currentIndex--) : (currentIndex++);
+		// Respond to interaction by updating message with new embed
+		await interaction.update({
+			content: 'Are you sure you want to remove duplicate tracks from the queue?```'+pages[currentIndex].join('\n')+'```\n(Reply to this message with y/n to confirm.)',
+			components: [
+				new MessageActionRow({
+					components: [
+						// back button if it isn't the start
+						...(currentIndex ? [backButton] : []),
+						// forward button if it isn't the end
+						...(currentIndex < pages.length-1 ? [forwardButton] : [])
+					]
+				})
+			]
+		});
+	});
+	queues[message.guild.id].events.on('update', () => { collector.stop(); });
+
+
 
 	const filter = m => message.author.id === m.author.id && m.type == 'REPLY';
 	function getChosenDecisionResult() {
